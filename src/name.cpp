80544d1250737be1832f90d9226cad52b4c3cc58
benclockworks <bbayor@gmail.com>
2023-10-06 12:13:27 -0400

    first commit

---
+++ src/name.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+void askUser(){
+    string theName;
+    cout<<"What is your name? :";
+    cin>>theName;
+    cout<<"Thanks for telling me your name "<<theName<<"."<<endl;
+    
+
+}
